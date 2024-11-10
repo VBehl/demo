4d2108fbc673821fdf5507fb6e6c81ea9338a352
VBehl <137898881+VBehl@users.noreply.github.com>
2024-11-10 22:27:43 +0530

    Add files via upload

---
+++ 2024_11_10_092106_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id("categoryId");
+            $table->string("name");
+            $table->string("parentCategory");
+            $table->string("desc");
+            $table->integer("totlbooks");
+            $table->boolean("isShow")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
